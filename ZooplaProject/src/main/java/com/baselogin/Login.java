2b0f13510689871b801d180f7f264e79b125bb55
shafr <shafr@10.0.0.15>
2020-05-06 21:30:00 -0400

    Initial Commit

---
+++ ZooplaProject/src/main/java/com/baselogin/Login.java
@@ -0,0 +1,46 @@
+package com.baselogin;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		// Initialize browserecho
+		//interface 
+		WebDriver driver=new ChromeDriver();//upcasting polymorphism
+		// Maximize browser
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);//implicit wait ==>HTML DOM page(slow down automation)
+		
+		driver.get("https://www.zoopla.co.uk/");
+		//driver.getTitle();
+		System.out.println(driver.getTitle());
+		
+		driver.findElement(By.xpath("//*[@class='ui-button-primary ui-cookie-accept-all-medium-large']")).click();
+		
+		driver.findElement(By.xpath("//*[@class='button button--tertiary-dark account-link__text']")).click();
+		
+		driver.findElement(By.xpath("//*[@type='email']")).sendKeys("shafranshurid@gmail.com");
+		
+		driver.findElement(By.xpath("//*[@type='password']")).sendKeys("Shafran9124730");
+		
+		driver.findElement(By.xpath("//*[@id='signin_submit']")).click();
+		
+		System.out.println(driver.getTitle());
+		
+		//driver.quit();
+		
+		
+		
+		
+	}
+
+}
